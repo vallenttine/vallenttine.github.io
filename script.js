0ad2345f827ae5279ef9ae528214871c0334fc18
klasterr <kuzjovictor@gmail.com>
2024-02-13 20:44:39 +0200

    valentine2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var buttonChangedTimes = 0;
 var buttonNO = document.getElementById("Nobutton");
+var textChild = document.getElementById("text");
 buttonNO.addEventListener("click", () => {
     var screenWidth = window.innerWidth; 
     var screenHeight = window.innerHeight; 
@@ -20,12 +21,14 @@ buttonNO.addEventListener("click", () => {
     switch (buttonChangedTimes){
         case 0:
             moveButton()
+            textChild.textContent = "Не правильна відовідь!";
             break;
         case 1:
             moveButton()
             changeMinWidth()
             buttonNO.style.fontSize = "16px";
             buttonNO.style.padding = "0 16px";
+            textChild.textContent = "Неподобство!!!";
             break;
         case 2:
             moveButton()
@@ -34,6 +37,7 @@ buttonNO.addEventListener("click", () => {
             buttonNO.style.lineHeight = "32px";
             buttonNO.style.padding = "0 14px";
             buttonNO.style.boxShadow = "#422800 2px 2px 0 0";
+            textChild.textContent = "То що таке має бути?!";
             break;
         case 3:
             moveButton()
@@ -42,6 +46,7 @@ buttonNO.addEventListener("click", () => {
             buttonNO.style.lineHeight = "24px";
             buttonNO.style.padding = "0 12px";
             buttonNO.style.boxShadow = "#422800 1px 1px 0 0";
+            textChild.textContent = "Софія 😠";
             break;
         case 4:
             moveButton()
@@ -50,13 +55,24 @@ buttonNO.addEventListener("click", () => {
             buttonNO.style.lineHeight = "18px";
             buttonNO.style.padding = "0 10px";
             buttonNO.style.boxShadow = "0 0 0 0";
+            textChild.textContent = "Наніц припухла? 🤨";
             break;
         case 5:
+            moveButton()
+            changeMinWidth()
+            buttonNO.style.fontSize = "8px";
+            buttonNO.style.lineHeight = "12px";
+            buttonNO.style.padding = "0 8px";
+            buttonNO.style.boxShadow = "0 0 0 0";
+            textChild.textContent = "Це обіда 🙁";
+            break;
+        case 6:
             buttonNO.style.left = screenWidth + "px";
             buttonNO.style.top = screenHeight + "px";
             setTimeout(function() {
                 buttonNO.style.visibility = "hidden";
             }, 100);
+            textChild.textContent = "Кнопка там 👇";
             break;
         }
 })
