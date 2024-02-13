422ca9bc533c7f7bd83bd65f61e859be864dd16d
klasterr <kuzjovictor@gmail.com>
2024-02-13 22:25:10 +0200

    valentine3

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var buttonChangedTimes = 0;
-var buttonNO = document.getElementById("Nobutton");
+var noButton = document.getElementById("noButton");
+var yesButton = document.getElementById("yesButton");
+var gif = document.getElementById("gif");
 var textChild = document.getElementById("text");
-buttonNO.addEventListener("click", () => {
+noButton.addEventListener("click", () => {
     var screenWidth = window.innerWidth; 
     var screenHeight = window.innerHeight; 
 
@@ -9,14 +11,14 @@ buttonNO.addEventListener("click", () => {
     var randomY = Math.floor(Math.random() * (screenHeight - 80));
 
     function moveButton(){
-        buttonNO.style.position = "absolute";
-        buttonNO.style.left = randomX + "px";
-        buttonNO.style.top = randomY + "px";
+        noButton.style.position = "absolute";
+        noButton.style.left = randomX + "px";
+        noButton.style.top = randomY + "px";
         buttonChangedTimes++;
     }
     function changeMinWidth(){
-        var minWidth = getComputedStyle(buttonNO).getPropertyValue('min-width').slice(0, -2);
-        buttonNO.style.minWidth = (minWidth-20) + "px";
+        var minWidth = getComputedStyle(noButton).getPropertyValue('min-width').slice(0, -2);
+        noButton.style.minWidth = (minWidth-20) + "px";
     }
     switch (buttonChangedTimes){
         case 0:
@@ -26,55 +28,78 @@ buttonNO.addEventListener("click", () => {
         case 1:
             moveButton()
             changeMinWidth()
-            buttonNO.style.fontSize = "16px";
-            buttonNO.style.padding = "0 16px";
+            noButton.style.fontSize = "16px";
+            noButton.style.padding = "0 16px";
             textChild.textContent = "Неподобство!!!";
             break;
         case 2:
             moveButton()
             changeMinWidth()
-            buttonNO.style.fontSize = "14px";
-            buttonNO.style.lineHeight = "32px";
-            buttonNO.style.padding = "0 14px";
-            buttonNO.style.boxShadow = "#422800 2px 2px 0 0";
+            noButton.style.fontSize = "14px";
+            noButton.style.lineHeight = "32px";
+            noButton.style.padding = "0 14px";
+            noButton.style.boxShadow = "#422800 2px 2px 0 0";
             textChild.textContent = "То що таке має бути?!";
             break;
         case 3:
             moveButton()
             changeMinWidth()
-            buttonNO.style.fontSize = "12px";
-            buttonNO.style.lineHeight = "24px";
-            buttonNO.style.padding = "0 12px";
-            buttonNO.style.boxShadow = "#422800 1px 1px 0 0";
+            noButton.style.fontSize = "12px";
+            noButton.style.lineHeight = "24px";
+            noButton.style.padding = "0 12px";
+            noButton.style.boxShadow = "#422800 1px 1px 0 0";
             textChild.textContent = "Софія 😠";
             break;
         case 4:
             moveButton()
             changeMinWidth()
-            buttonNO.style.fontSize = "10px";
-            buttonNO.style.lineHeight = "18px";
-            buttonNO.style.padding = "0 10px";
-            buttonNO.style.boxShadow = "0 0 0 0";
+            noButton.style.fontSize = "10px";
+            noButton.style.lineHeight = "18px";
+            noButton.style.padding = "0 10px";
+            noButton.style.boxShadow = "0 0 0 0";
             textChild.textContent = "Наніц припухла? 🤨";
             break;
         case 5:
             moveButton()
             changeMinWidth()
-            buttonNO.style.fontSize = "8px";
-            buttonNO.style.lineHeight = "12px";
-            buttonNO.style.padding = "0 8px";
-            buttonNO.style.boxShadow = "0 0 0 0";
+            noButton.style.fontSize = "8px";
+            noButton.style.lineHeight = "12px";
+            noButton.style.padding = "0 8px";
+            noButton.style.boxShadow = "0 0 0 0";
             textChild.textContent = "Це обіда 🙁";
             break;
         case 6:
-            buttonNO.style.left = screenWidth + "px";
-            buttonNO.style.top = screenHeight + "px";
+            noButton.style.left = screenWidth + "px";
+            noButton.style.top = screenHeight + "px";
             setTimeout(function() {
-                buttonNO.style.visibility = "hidden";
+                noButton.style.visibility = "hidden";
             }, 100);
             textChild.textContent = "Кнопка там 👇";
             break;
         }
 })
+yesButton.addEventListener("click", () => {
+    gif.src = "https://media1.tenor.com/m/XAabbheOS24AAAAC/goma-happy.gif";
+    textChild.textContent = "Ура ура ура!";
+    document.getElementsByClassName("buttons")[0].style.display = "none";
+    textChild.style.paddingTop = "8px";
+    textChild.style.fontSize = "20px";
+    var gifs = [
+        "https://media1.tenor.com/m/jD8fmhulF4MAAAAC/clap-clap-cute-cat.gif",
+        "https://media1.tenor.com/m/VOd9bvqdPqAAAAAC/f.gif",
+        "https://media1.tenor.com/m/UZMDKcNeUj0AAAAC/goma-peach-kisses.gif",
+        "https://media1.tenor.com/m/XAabbheOS24AAAAC/goma-happy.gif"
+      ];
+    
+    var interval = 4300;     
+    var index = 0;
 
+    function changeGif() {
+    gif.src = gifs[index];
+    index = (index + 1) % gifs.length;
+    }
+
+    var intervalId = setInterval(changeGif, interval);
+    gif.style.width = "20vh"
+})
 
